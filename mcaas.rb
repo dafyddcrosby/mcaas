ce5a0916004be98798c4e976d0edcc82ee6302fc
David Crosby <dave@dafyddcrosby.com>
2022-08-14 18:47:23 -0600

    RuboCop cleanup

--- mcaas.rb
+++ mcaas.rb
@@ -1,12 +1,14 @@
-require 'sinatra'
-require 'marky_markov'
+# frozen_string_literal: true
 
-get '/' do
+require "sinatra"
+require "marky_markov"
+
+get "/" do
   "Welcome to MCaaS"
 end
 
-get '/demo/:add' do
-  markov = MarkyMarkov::Dictionary.new('dictionary') # Saves/opens dictionary.mmd
+get "/demo/:add" do
+  markov = MarkyMarkov::Dictionary.new("dictionary") # Saves/opens dictionary.mmd
   markov.parse_string(params[:add])
   markov.save_dictionary!
   return markov.generate_n_sentences 5
